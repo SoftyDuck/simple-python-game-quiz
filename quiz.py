34db2ffcde2b0a114440abe73fb72a2688babf93
sus <softyduck@icloud.com>
2022-06-21 20:19:43 +0100

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,7 +1,10 @@
 #Quiz by SoftyDuck
+from curses import color_content
 import time
 #Variables
 score = 0
+correct = "You are correct! 1 point has been added!"
+
 print("Score: " + str(score))
 print("Welcome to Game Quiz!")
 time.sleep(3)
@@ -17,7 +20,7 @@ print("C is fortnite")
 question_1 = input("Choose one from above (A, B or C): ")
 
 if question_1== "B":
-    print("You are correct! 1 Point has been added")
+    print(correct)
     score = score + 1
 else:
     print("You got the question wrong. The correct answer as B. 1 Point removed")
@@ -34,7 +37,7 @@ print("C: a shooter game.")
 question_2 = input("Choose one from above (A, B or C): ")
 
 if question_2== "A":
-    print("You are correct! 1 point has been added!")
+    print(correct)
     score = score + 1
 else:
     print("You got the question wrong. The correct answer was A. 1 Point removed.")
@@ -51,7 +54,7 @@ print("C: a simple sandbox game where you can build what you want!")
 question_3 = input("Choose one from above (A, B or C): ")
 
 if question_3== "C":
-    print("You are correct! 1 point has been added!")
+    print(correct)
     score = score + 1
 else:
     print("You got the question wrong. The correct answer was A. 1 Point removed.")
@@ -67,11 +70,30 @@ print("B: No")
 question_4 = input("Choose one from above (A or B): ")
 
 if question_4== "A":
-    print("You are correct! 1 point has been added!")
+    print(correct)
     score = score + 1
 else:
     print("You got it wrong :(, I'm taking 1 point")
     score = score - 1
 
+#question 5
+print("Question 5:")
+print("Which best describes Fortnite?")
+time.sleep(3)
+print("A: a sandbox game")
+print("B: a battle royal shooter game with building.")
+print("C: a horror game")
+question_5 = input("Choose one from above (A or B): ")
+
+if question_5== "B":
+    print(correct)
+    score = score + 1
+else:
+    print("You got the question wrong! 1 point has been removed.")
+    score = score -1
+
 #Ending
 print("Thank you for taking this quiz. You got the score of: " + str(score))
+time.sleep(4)
+print("Closing Quiz...")
+time.sleep(2)
