cc8b394c047874bcae03ee93f5e2ff177fdfed3d
sus <softyduck@icloud.com>
2022-07-02 10:33:54 +0100

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,7 +1,8 @@
 #Quiz by SoftyDuck
 from curses import color_content
 import time
-#Variables
+
+# Variables
 score = 0
 correct = "You are correct! 1 point has been added!"
 
@@ -11,7 +12,7 @@ time.sleep(3)
 print("Lets begin!")
 time.sleep(2)
 
-#Question One
+# Question One
 print("Question 1:")
 print("Which game is the best game?")
 print("A: Minecraft")
@@ -19,32 +20,39 @@ print("B = Among us")
 print("C is fortnite")
 question_1 = input("Choose one from above (A, B or C): ")
 
-if question_1== "B":
+if question_1 == "B" or "b":
     print(correct)
     score = score + 1
-else:
+elif question_1=="A" or "a" or "B" or "b":
     print("You got the question wrong. The correct answer as B. 1 Point removed")
+    score = score - 1
+else:
+    print("That is an invalid answer. The correct answer as B. 1 Point removed")
     score = score - 1
 time.sleep(3)
 
-#Question 2
+# Question 2
 print("Question 2:")
 print("Which best describes Among Us?")
 time.sleep(2)
-print("A: an game where you have to find the imposter. You can compelete tasks. You turn into a ghost when you get killed. If your an imposter, you have to kill everyone.")
+print("A: an game where you have to find the imposter. You can compelete tasks. You turn into a ghost when you get "
+      "killed. If your an imposter, you have to kill everyone.")
 print("B: a happy game where everyone has fun!")
 print("C: a shooter game.")
 question_2 = input("Choose one from above (A, B or C): ")
 
-if question_2== "A":
+if question_2 == "A" or "a":
     print(correct)
     score = score + 1
-else:
+elif question_2=="B" or "b" or "C" or "c":
     print("You got the question wrong. The correct answer was A. 1 Point removed.")
+    score = score - 1
+else:
+    print("Thats an invalid answer. The correct answer was A. 1 Point removed.")
     score = score - 1
 time.sleep(3)
 
-#Question 3
+# Question 3
 print("Question 3:")
 print("What is Minecraft?")
 time.sleep(3)
@@ -53,15 +61,18 @@ print("B: a battle royal game simular to Fortnite")
 print("C: a simple sandbox game where you can build what you want!")
 question_3 = input("Choose one from above (A, B or C): ")
 
-if question_3== "C":
+if question_3 == "C" or "c":
     print(correct)
     score = score + 1
-else:
+elif question_3=="A" or "a" or "B" or "b":
     print("You got the question wrong. The correct answer was A. 1 Point removed.")
+    score = score - 1  
+else:
+    print("Thats an invalid answer. The correct answer was A. 1 Point removed.")
     score = score - 1
 time.sleep(3)
 
-#Question 4
+# Question 4
 print("Question 4:")
 print("is the imposter sus?")
 time.sleep(2)
@@ -69,14 +80,17 @@ print("A: Yes")
 print("B: No")
 question_4 = input("Choose one from above (A or B): ")
 
-if question_4== "A":
+if question_4 == "A" or "a":
     print(correct)
     score = score + 1
+elif question_4== "B" or "b" or "C" or "c":
+        print("You got it wrong :(, I'm taking 1 point")
+    score = score - 1
 else:
-    print("You got it wrong :(, I'm taking 1 point")
+    print("Thats an invalid answer, I'm taking 1 point")
     score = score - 1
 
-#question 5
+# question 5
 print("Question 5:")
 print("Which best describes Fortnite?")
 time.sleep(3)
@@ -85,15 +99,21 @@ print("B: a battle royal shooter game with building.")
 print("C: a horror game")
 question_5 = input("Choose one from above (A or B): ")
 
-if question_5== "B":
+if question_5 == "B" or "b":
     print(correct)
     score = score + 1
-else:
+elif question_5== "A" or "a" or "C" or "c":
     print("You got the question wrong! 1 point has been removed.")
-    score = score -1
+    score = score - 1  
+else:
+    print("Thats an Invalid answer. 1 point has been removed.")
+    score = score - 1
+# Ending
 
-#Ending
-print("Thank you for taking this quiz. You got the score of: " + str(score))
+def congrats():
+    print("Thank you for taking this quiz. You got the score of: " + str(score))
+
+congrats()
 time.sleep(4)
 print("Closing Quiz...")
 time.sleep(2)
